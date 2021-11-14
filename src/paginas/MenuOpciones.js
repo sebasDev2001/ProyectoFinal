2c33016633d590d14d23830c1134c0e9aa7e8985
sebasDev2001 <sebastian.sierrav2001@gmail.com>
2021-11-14 15:28:52 -0500

    Se añadieron las pruebas de UI y se arreglaron errores ortograficos

--- src/paginas/MenuOpciones.js
+++ src/paginas/MenuOpciones.js
@@ -14,13 +14,10 @@ function MenuOpciones(){
                 <button onClick={()=>{history.push('/fibonacci')}}>Calcular Fibonacci</button>
                 <button onClick={()=>{history.push('/factorial')}}>Calcular Factoriales</button>
                 <button onClick={()=>{history.push('/triangulo')}}>Area y Perimetro de Triangulo</button>
-                <button onClick={()=>{history.push('/circulo')}}>Area y Perimetro de circulo</button>
-                <button onClick={()=>{history.push('/rectangulo')}}>Area y Perimetro de rectangulo</button>
+                <button onClick={()=>{history.push('/circulo')}}>Area y Perimetro de Circulo</button>
+                <button onClick={()=>{history.push('/rectangulo')}}>Area y Perimetro de Rectangulo</button>
             </div>
         </div>    
     )
 }
-
-
-
 export default MenuOpciones;
